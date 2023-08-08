a2cdf08c5bb487bfdc1468fafb69b318dadd29d3
Diego Huarsaya <130510885+Alaskechufles@users.noreply.github.com>
2023-08-08 07:28:23 -0500

    Terminado

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $mysqli = new mysqli("localhost", "root", "", "prueba_pf_2");
+    $mysqli = new mysqli("localhost", "root", "", "db_project_3");
 } catch (mysqli_sql_exception $e) {
     echo "Error:" . $e->getMessage();
 }
